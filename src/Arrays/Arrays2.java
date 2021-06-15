f05ab8cccede74cbbd5ad0f42f0f0346d212ec00
Gabrielbemo <gabrielbemoura@gmail.com>
2021-06-15 17:41:12 -0300

    Add Arrays2, foreach usage

---
+++ src/Arrays/Arrays2.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Arrays2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        int[] numbers2 = {1,2,3,4,5};
+        int[] numbers3 = new int[]{1,2,3,4,5};
+
+        for (int iterator = 0; iterator < numbers2.length; iterator++){
+            System.out.println(numbers2[iterator]);
+        }
+
+        for(int number : numbers2){
+            System.out.println(number);
+        }
+    }
+}
