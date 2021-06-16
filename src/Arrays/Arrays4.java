cbc12dffb64ae2bbdf751d3a2321df7d9d334c9f
Gabrielbemo <gabrielbemoura@gmail.com>
2021-06-16 14:45:49 -0300

    Add Arrays4/Multidimensional Arrays with different dimensions

---
+++ src/Arrays/Arrays4.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Arrays4 {
+    public static void main(String[] args) {
+//        int[][] days = new int[3][];
+//        days[0] = new int[2];
+//        days[1] = new int[]{1, 2, 3};
+//        days[2] = new int[4];
+
+        int[][] days = {{0,0},{1,2,3},{0,0,0,0}};
+
+        for (int[] arr : days) {
+            System.out.println("array:" + arr);
+            for (int num : arr) {
+                System.out.println(num);
+            }
+        }
+    }
+}
