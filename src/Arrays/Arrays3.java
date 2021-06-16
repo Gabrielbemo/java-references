bb89ccd9e298fd9ac92de7fb6d6b2edc947454fc
Gabrielbemo <gabrielbemoura@gmail.com>
2021-06-16 14:31:55 -0300

    Add Arrays3/Multidimensional Arrays

---
+++ src/Arrays/Arrays3.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class Arrays3 {
+    public static void main(String[] args) {
+        int[][] days = new int[2][2];
+        days[0][0] = 30;
+        days[0][1] = 31;
+        days[1][0] = 29;
+        days[1][1] = 28;
+
+        for (int index = 0; index<days.length; index++){
+            System.out.println("endereço de memória: " + days[index]);
+            for (int index2 = 0; index2<days.length; index2++){
+                System.out.println(days[index][index2]);
+            }
+        }
+
+        System.out.println("-------------------------------");
+
+        //foreach
+        for (int[] ref : days) {
+            for (int dia: ref){
+                System.out.println("Day: " + dia);
+            }
+        }
+
+    }
+}
