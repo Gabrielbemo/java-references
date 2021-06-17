8972f18d9fc6ea0f838014cfa31c47573914ca20
Gabrielbemo <gabrielbemoura@gmail.com>
2021-06-17 15:13:45 -0300

    add methods/ Calculator

---
+++ src/br/com/java/javacore/methods/Calculator.java
@@ -0,0 +1,11 @@
+package br.com.java.javacore.methods;
+
+public class Calculator {
+    public void sumTwoNumbers(){
+        System.out.println(5+5);
+    }
+
+    public void subtractTwoNumbers(){
+        System.out.println(5-5);
+    }
+}
---
+++ src/br/com/java/javacore/methodstest/CalculatorTest.java
@@ -0,0 +1,11 @@
+package br.com.java.javacore.methodstest;
+
+import br.com.java.javacore.methods.Calculator;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.sumTwoNumbers();
+        calculator.subtractTwoNumbers();
+    }
+}
